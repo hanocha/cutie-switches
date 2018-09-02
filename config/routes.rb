31eb9be93ff60ed477459e469e1d5badde58112d
hanocha <hanocha.peter@gmail.com>
2018-09-02 21:09:55 +0900

    $ rails g controller Switches index show update create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'switches/index'
+  get 'switches/show'
+  get 'switches/update'
+  get 'switches/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
