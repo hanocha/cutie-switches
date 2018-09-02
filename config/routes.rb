2e2dec2f5409150e8caea6e9dd64e8bcb7d6ab16
hanocha <hanocha.peter@gmail.com>
2018-09-02 21:48:29 +0900

    Fix routing for switch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'switches/index'
-  get 'switches/show'
-  get 'switches/update'
-  get 'switches/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :switches
 end
