9b1be214294233f04092a4f11386069276ca39e8
hanocha <hanocha.peter@gmail.com>
2018-09-12 00:17:09 +0900

    Create minimum Image component

--- app/javascript/src/components/pages/App/index.jsx
+++ app/javascript/src/components/pages/App/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Ordinary } from '../../layouts/Ordinary';
+import { Image } from '../../atoms/Image';
 import style from './App.scss';
 
 export const App = props => (
   <Ordinary>
-    <div className={style.myHeadline}>Hello React!</div>
+    <Image src='http://cherryamericas.com/wp-content/uploads/2017/04/MX-Silent-Red.jpg' />
   </Ordinary>
 );
