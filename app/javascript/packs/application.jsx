774715ecaa9d3956ebf342510e5680f097054a91
hanocha <hanocha.peter@gmail.com>
2018-09-11 23:31:41 +0900

    Create and use App component

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import { App } from '../src/components/pages/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
