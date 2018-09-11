444f9d1ccd4f4f78be29fac48ded1a908ac1cbf8
hanocha <hanocha.peter@gmail.com>
2018-09-12 00:23:37 +0900

    Create Switch component

---
+++ app/javascript/src/components/molecules/Switch/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Image } from '../../atoms/Image';
+
+export const Switch = () => (
+  <div>
+    <Image src='http://cherryamericas.com/wp-content/uploads/2017/04/MX-Silent-Red.jpg' />
+  </div>
+);
--- app/javascript/src/components/pages/App/index.jsx
+++ app/javascript/src/components/pages/App/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Ordinary } from '../../layouts/Ordinary';
-import { Image } from '../../atoms/Image';
+import { Switch } from '../../molecules/Switch';
 import style from './App.scss';
 
 export const App = props => (
   <Ordinary>
-    <Image src='http://cherryamericas.com/wp-content/uploads/2017/04/MX-Silent-Red.jpg' />
+    <Switch />
   </Ordinary>
 );
