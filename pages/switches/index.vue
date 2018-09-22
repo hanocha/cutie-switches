921af14377bee3974f38bc68df06d7aa8db612d5
hanocha <hanocha.peter@gmail.com>
2018-09-22 16:32:06 +0900

    Show switches in index page

--- pages/switches/index.vue
+++ pages/switches/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    test
+  <div class='myContainer'>
+    <div class='mySwitchContainer'>
+      <img src='http://www.kailh.com/uploads/180605/2-1P60514211HI.jpg' class='mySwitchImage'>
+    </div>
+    <div class='mySwitchContainer'>
+      <img src='http://www.kailh.com/uploads/allimg/180605/2-1P605143J60-L.jpg' class='mySwitchImage'>
+    </div>
+    <div class='mySwitchContainer'>
+      <img src='http://www.kailh.com/uploads/180605/2-1P6051029105Q.jpg' class='mySwitchImage'>
+    </div>
   </div>
 </template>
 
@@ -9,6 +17,24 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.my {
+  &Container {
+    display: flex;
+  }
 
-<style lang="sass">
+  &Switch {
+    &Container {
+      margin: 16px;
+      padding: 16px;
+      border: solid 2px black;
+      border-radius: 8px;
+    }
+
+    &Image {
+      width: 128px;
+      height: 128px;
+    }
+  }
+}
 </style>
