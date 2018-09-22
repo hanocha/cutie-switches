9a2d65259438b15e3741cfe640172e7a7672ba5d
hanocha <hanocha.peter@gmail.com>
2018-09-22 16:19:40 +0900

    Add header component and render it all pages

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='myContainer'>
+    <h1 class='myHeadline'>cutie-switches</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='scss' scoped>
+.my {
+  &Container {
+    display: flex;
+    height: 64px;
+  }
+
+  &Headline {
+    margin: auto;
+  }
+}
+</style>
