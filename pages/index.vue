e030ea0fb6f4bc2aa9c8f63009ce7ce2b07b4137
hanocha <hanocha.peter@gmail.com>
2018-09-22 16:19:18 +0900

    Delete default AppLogo component

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,13 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        cutie-switches
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <h1 class="title">
+      cutie-switches
+    </h1>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
 }
 </script>
 
@@ -62,4 +41,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-
