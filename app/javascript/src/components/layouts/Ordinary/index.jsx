61488fb6988fbb5437881eb80afad2e3e874c0e6
hanocha <hanocha.peter@gmail.com>
2018-09-22 10:58:01 +0900

    reset

--- app/javascript/src/components/layouts/Ordinary/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './Ordinary.scss';
-
-export const Ordinary = ({ children }) => (
-  <div className={style.my}>
-    {children}
-  </div>
-);
--- app/javascript/src/components/molecules/Switch/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Image } from '../../atoms/Image';
-
-export const Switch = () => (
-  <div>
-    <Image src='http://cherryamericas.com/wp-content/uploads/2017/04/MX-Silent-Red.jpg' />
-  </div>
-);
