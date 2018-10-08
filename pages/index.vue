8f9cdfc224b7dfdf4cbdab7ad156bff8a8642921
hanocha <hanocha.peter@gmail.com>
2018-10-08 21:53:56 +0900

    Add switches

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,50 @@
 <template>
   <div class='myContainer'>
-    <switch-component img-src='cherry-mx-green.png' sound-src='cherry-mx-green.mp3' />
-    <switch-component img-src='cherry-mx-gray.png' sound-src='cherry-mx-gray.mp3' />
-    <switch-component img-src='cherry-mx-red.png' sound-src='cherry-mx-red.mp3' />
+    <switch-component
+      name='ALPS blue'
+      img-src='alps_blue.png'
+      sound-src='alps_blue.mp3'
+    />
+    <switch-component
+      name='ALPS hulk'
+      img-src='alps_hulk.png'
+      sound-src='alps_hulk.mp3'
+    />
+    <switch-component
+      name='ALPS white'
+      img-src='alps_white.png'
+      sound-src='alps_white.mp3'
+    />
+    <switch-component
+      name='Cherry MX silent red'
+      img-src='cherry_silent_red.png'
+      sound-src='cherry_silent_red.mp3'
+    />
+    <switch-component
+      name='Kailh box pink'
+      img-src='kailh_box_pink.png'
+      sound-src='kailh_box_pink.mp3'
+    />
+    <switch-component
+      name='Kailh box royal'
+      img-src='kailh_box_royal.png'
+      sound-src='kailh_box_royal.mp3'
+    />
+    <switch-component
+      name='Kailh box violet'
+      img-src='kailh_box_violet.png'
+      sound-src='kailh_box_violet.mp3'
+    />
+    <switch-component
+      name='Kailh box white'
+      img-src='kailh_box_white.png'
+      sound-src='kailh_box_white.mp3'
+    />
+    <switch-component
+      name='Kailh pink'
+      img-src='kailh_pink.png'
+      sound-src='kailh_pink.mp3'
+    />
   </div>
 </template>
 
@@ -21,7 +63,8 @@ export default {
 .my {
   &Container {
     display: flex;
-    justify-content: space-around;
+    flex-wrap: wrap;
+    justify-content: space-between;
   }
 
   &Switch {
